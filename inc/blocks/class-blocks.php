819aeaf6332463dc23ac5b1abb8f1094c0cc842f
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2022-09-21 16:54:03 +0900

    ブロックCSS読み込み形式変更

--- inc/blocks/class-blocks.php
+++ inc/blocks/class-blocks.php
@@ -66,6 +66,13 @@ class Blocks {
 		}
 	}
 
+	/**
+	 * 子テーマ利用時のScript,CSS読み込み調整
+	 *
+	 * @param array $metadata Meta Data.
+	 *
+	 * @return array
+	 */
 	public function block_type_metadata( $metadata ) {
 		// odango 利用であれば調整の必要なし.
 		if ( ! is_child_theme() ) {
@@ -148,24 +155,25 @@ class Blocks {
 	public function enqueue_theme_block_styles() {
 
 		$this->enqueue_block_styles(
-			get_template_directory() . '/assets/css/core-blocks',
-			'core'
+			get_template_directory() . '/assets/css/block-styles',
 		);
 		// 子テーマも検索.
 		if ( is_child_theme() ) {
-			$this->enqueue_block_styles(
-				get_stylesheet_directory() . '/assets/css/core-blocks',
-				'core',
-				true
+			$dir_path = apply_filters(
+				'ooo_block_styles_dir_path',
+				get_stylesheet_directory() . '/assets/css/block-styles'
 			);
+			$this->enqueue_block_styles( $dir_path, true );
 		}
 	}
 
 
-	private function enqueue_block_styles( $dir, $namespace, $is_child = false ) {
+	private function enqueue_block_styles( $dir, $is_child = false ) {
 		foreach ( glob( "${dir}/*", GLOB_ONLYDIR ) as $dir_path ) {
-			$block_name     = $namespace . '/' . basename( $dir_path );
-			$theme_css_path = $dir_path . '/' . basename( $dir_path ) . '.css';
+			$block          = $this->parse_block_name( basename( $dir_path ) );
+			$block_name     = $block['namespace'] . '/' . $block['name'];
+			$theme_css_path = $dir_path . '/' . $block['name'] . '.css';
+
 			wp_enqueue_block_style(
 				$block_name,
 				[
@@ -177,6 +185,21 @@ class Blocks {
 		}
 	}
 
+
+	private function parse_block_name( $name ) {
+		$block_name = explode( '__', $name );
+		$namespace  = 'core';
+		if ( isset( $block_name[1] ) && ! empty( $block_name[1] ) ) {
+			$namespace     = $block_name[0];
+			$block_name[0] = '';
+		}
+		$block_name = implode( '__', array_filter( $block_name ) );
+
+		return [
+			'namespace' => $namespace,
+			'name'      => $block_name,
+		];
+	}
 
 	/**
 	 * ブロックのスタイル追加
